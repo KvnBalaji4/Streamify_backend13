e2921e6fd55e1f588ebcc0b4d5bb739df85f57dd
KvnBalaji4 <venkatanagabalaji124@gmail.com>
2025-11-05 08:31:16 +0530

    Update CrossOrigin URL in UserController

--- src/main/java/com/demo/UserController.java
+++ src/main/java/com/demo/UserController.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:6085")
 public class UserController {
 
     @Autowired
